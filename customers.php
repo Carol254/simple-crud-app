9aa0c4b572132b141a9e15a3f852ecab651b3aa9
Caroline <wanguic49@gmail.com>
2024-04-09 14:49:05 +0300

    few changes made

--- customers.php
+++ customers.php
@@ -3,58 +3,64 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
 <body>
-    <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "myCustomers";
+    <h3 style="text-align: center; text-decoration:underline;">CUSTOMER DATA</h3>
+    <div class="main">
+        
+        <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "carolkui";
+            $dbname = "myCustomers";
 
-        $conn = new mysqli($servername, $username, $password ,$dbname);
+            $conn = new mysqli($servername, $username, $password ,$dbname);
 
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-          }
-          echo "Connected successfully";
-
-        //  $customers = "CREATE DATABASE myCustomers";
-         
-        //  if($conn->query($customers) === TRUE){
-        //     echo "Database created successfully";
-        //  }else{
-        //     echo "Error creating Database" .$conn->error;
-        //  }
-
-        // $sql = "CREATE TABLE customerDetails(
-        //     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        //     firstname VARCHAR(30) NOT NULL,
-        //     lastname VARCHAR(30) NOT NULL,
-        //     email VARCHAR(50),
-        //     age INT(4) NOT NULL,
-        //     gender VARCHAR(30) NOT NULL
-        // )";
-
-
-        $sql = "SELECT id,firstname,lastname,email,age,gender FROM customerDetails";
-        $results = $conn->query($sql);
-
-        if($results->num_rows > 0){
-            echo "<table><tr><th>ID</th><th>Firstname</th><th>LastName</th><th>Email</th><th>Age</th><th>Gender</th></tr>";
-
-            while($row = $results->fetch_assoc()){
-                echo "<tr><td>".$row["id"] ."</td><td>".$row["firstname"] ."</td><td>".$row["lastname"] ."</td><td>".$row["email"] ."</td><td>".$row["age"] ."</td><td>".$row["gender"] ."</td></tr>";
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
             }
-            echo "</table>";
-         }else{
-            echo "0 results";
-        }
+            // echo "Connected successfully";
+
+            //  $customers = "CREATE DATABASE myCustomers";
+            
+            //  if($conn->query($customers) === TRUE){
+            //     echo "Database created successfully";
+            //  }else{
+            //     echo "Error creating Database" .$conn->error;
+            //  }
+
+            // $sql = "CREATE TABLE customerDetails(
+            //     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            //     firstname VARCHAR(30) NOT NULL,
+            //     lastname VARCHAR(30) NOT NULL,
+            //     email VARCHAR(50),
+            //     age INT(4) NOT NULL,
+            //     gender VARCHAR(30) NOT NULL
+            // )";
+
+
+            $sql = "SELECT id,firstname,lastname,email,age,gender FROM customerDetails";
+            $results = $conn->query($sql);
+
+            if($results->num_rows > 0){
+                echo "<table><tr><th>ID</th><th>Firstname</th><th>LastName</th><th>Email</th><th>Age</th><th>Gender</th></tr>";
+
+                while($row = $results->fetch_assoc()){
+                    echo "<tr><td>".$row["id"] ."</td><td>".$row["firstname"] ."</td><td>".$row["lastname"] ."</td><td>".$row["email"] ."</td><td>".$row["age"] ."</td><td>".$row["gender"] ."</td></tr>";
+                }
+                echo "</table>";
+            }else{
+                echo "0 results";
+            }
 
 
 
+        
+            $conn->close();
+        ?>
+    </div>
     
-        $conn->close();
-    ?>
 </body>
 </html>
